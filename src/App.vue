9a52ac7865117d45531200da0eb92c40036aada8
JJ <jj@JJde-MacBook-Pro.local>
2020-02-25 21:36:03 +0800

    Add footer
    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <template template lang="pug">
 #app
+  toolbar
   router-view
+  credits
 </template>
+
+<script>
+import toolbar from '@/components/_toolbar.vue'
+import credits from '@/components/_credits.vue'
+
+export default {
+  mounted () {
+  },
+  data () {
+    return {
+    }
+  },
+  components: {
+    toolbar,
+    credits
+  }
+}
+</script>
 
 <style lang="scss">
 @import '@/assets/scss/main';
