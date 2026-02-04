4e0a15f2b7d577f83a2ae0a20c64795265ea7e2d
JJ <jerry7722@hotmail.com>
2026-02-04 20:51:40 +0800

    add GA

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import VueGtag from 'vue-gtag'
+
 Vue.config.productionTip = false
+
+Vue.use(VueGtag, {
+  config: { id: 'G-LC3YHK9MJH' }
+}, router)
 
 new Vue({
   router,
